4c5b1704c15cbd530f55b29352a8156c3ee492d8
Zack Muenz <zackmuenz@gmail.com>
2018-04-18 19:11:27 -0500

    Updated server

--- server.js
+++ server.js
@@ -14,11 +14,35 @@ var data = {
 var visitorCount = 0;
 
 // Create a set of routes for getting and posting table data
+app.get("/api/:tables?", function (req, res) {
+    var chosen = req.params.resrevations;
 
+    if (chosen) {
+        console.log(chosen);
+
+        for (var x = 0; x < tables.length; x++) {
+            if (chosen === tables[x].routeName) {
+                return res.json(tables[x]);
+            }
+        }
+        return res.json(false);
+    }
+    return res.json(tables);
+});
+
+app.post("/api/new", function(req, res) {
+    var newTable = req.body;
+ 
+    newTable.routeName = newTable.name;
+ 
+    console.log(newTable);
+ 
+    tables.push(newTable);
+});
 
 // Create a set of routes for displaying the HTML pages
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+        res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/reservation", function (req, res) {
