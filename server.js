a8cc5209de91c6f8306d8b0e6d2973c2f4ab5f81
Zack Muenz <zackmuenz@gmail.com>
2018-04-18 19:22:28 -0500

    Added on submit button click

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3600;
 
 // Create a few array variables that will hold the data
 var data = {
@@ -15,7 +15,7 @@ var visitorCount = 0;
 
 // Create a set of routes for getting and posting table data
 app.get("/api/:tables?", function (req, res) {
-    var chosen = req.params.resrevations;
+    var chosen = req.params.reservations;
 
     if (chosen) {
         console.log(chosen);
@@ -30,19 +30,23 @@ app.get("/api/:tables?", function (req, res) {
     return res.json(tables);
 });
 
-app.post("/api/new", function(req, res) {
+app.post("/api/new", function (req, res) {
     var newTable = req.body;
- 
+
     newTable.routeName = newTable.name;
- 
+
     console.log(newTable);
- 
-    tables.push(newTable);
+
+    if (data.reservations.length > 5) {
+        data.waitList.push(newTable);
+    } else {
+        data.reservations.push(newTable);
+    }
 });
 
 // Create a set of routes for displaying the HTML pages
 app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/reservation", function (req, res) {
@@ -55,4 +59,21 @@ app.get("/tables", function (req, res) {
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
+});
+
+$("#submit-btn").on("click", function (event) {
+    event.preventDefault();
+    var newTable = {
+        name: $("#name").val().trim(),
+        phone: $("#phone").val().trim(),
+        email: $("#email").val().trim(),
+        uniqueID: $("#UniqueID").val().trim()
+    };
+
+    // Question: What does this code do??
+    $.post("/api/new", newTable)
+        .then(function (data) {
+            console.log(data);
+            alert("Adding character...");
+        });
 });
